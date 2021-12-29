33db5eee4123efc9cac4a5529c7368bd71477fcf
Marta Zazlak <marta.zazlak@hotmail.com>
2021-12-29 10:18:10 +0100

    logic

--- AdvancedActivity/app/src/main/java/com/ndv/advancedactivity/MainActivity.kt
+++ AdvancedActivity/app/src/main/java/com/ndv/advancedactivity/MainActivity.kt
@@ -2,10 +2,69 @@ package com.ndv.advancedactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
+
+private const val TIME_UNIT = 60
+private const val SECONDS_IN_HOUR = TIME_UNIT * TIME_UNIT
+private const val DELAY_MS: Long = 1000;
+
+fun prepareTimeToDisplay(currentSeconds: Int): String {
+    val hours = currentSeconds / SECONDS_IN_HOUR
+    val minutes = (currentSeconds % TIME_UNIT) / TIME_UNIT
+    val seconds = currentSeconds % TIME_UNIT
+    return String.format("%d:%02d:%02d", hours, minutes, seconds)
+}
 
 class MainActivity : AppCompatActivity() {
+    private var currentSeconds: Int = 0;
+    private var isTimerRunning = false;
+
+    private fun updateTimer() {
+        val timer = findViewById<TextView>(R.id.timer)
+        timer.text = prepareTimeToDisplay(currentSeconds)
+    }
+
+    private fun startTimer() {
+        val handler = Handler(Looper.getMainLooper())
+        val updateTimerRunnable = object: Runnable {
+            override fun run() {
+                updateTimer()
+                if (isTimerRunning) {
+                    handler.postDelayed(this, DELAY_MS)
+                    currentSeconds++
+                }
+            }
+        }
+
+        handler.post(updateTimerRunnable)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.start)
+        val stopButton = findViewById<Button>(R.id.stop)
+        val clearButton = findViewById<Button>(R.id.clear)
+
+
+        startButton.setOnClickListener {
+            isTimerRunning = true
+            startTimer()
+        }
+
+        stopButton.setOnClickListener {
+            isTimerRunning = false
+        }
+
+        clearButton.setOnClickListener {
+            isTimerRunning = false
+            currentSeconds = 0
+        }
     }
 }
