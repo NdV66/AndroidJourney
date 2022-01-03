61593762aaa02dcdf342abd2ab0ba739dd74a753
Marta Zazlak <marta.zazlak@hotmail.com>
2022-01-03 12:22:02 +0100

    linear layouts - simple

--- Layouts/app/src/main/java/com/ndv/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/ndv/layouts/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.ndv.layouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val showHorizontalButton = findViewById<Button>(R.id.showLinearHorizontal)
+        val showLinearButton = findViewById<Button>(R.id.showLinearHorizontal)
+
+        showHorizontalButton.setOnClickListener {
+            val intent = Intent(this, LinearHorizontal::class.java)
+            startActivity(intent)
+        }
     }
 }
