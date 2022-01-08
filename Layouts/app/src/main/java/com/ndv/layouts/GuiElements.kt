763401dd63f5700fe9d3847ca579db770957cec9
Marta Zazlak <marta.zazlak@hotmail.com>
2022-01-08 11:29:28 +0100

    toast on select

--- Layouts/app/src/main/java/com/ndv/layouts/GuiElements.kt
+++ Layouts/app/src/main/java/com/ndv/layouts/GuiElements.kt
@@ -2,15 +2,34 @@ package com.ndv.layouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.RadioGroup
-import android.widget.ToggleButton
+import android.view.View
+import android.widget.*
 
 class GuiElements : AppCompatActivity() {
+    private fun showToast(text: String) {
+        val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gui_elements)
 
-        val switch = findViewById<RadioGroup>(R.id.radioGroup)
-        switch.checkedRadioButtonId
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val text = "I like ${spinner.selectedItem} too!";
+                showToast(text)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
     }
 }
