b12afe7183e79dbd40f247b9f125995da1bf535d
Marta Zazlak <marta.zazlak@hotmail.com>
2022-01-08 11:33:50 +0100

    refactor

--- Layouts/app/src/main/java/com/ndv/layouts/GuiElements.kt
+++ Layouts/app/src/main/java/com/ndv/layouts/GuiElements.kt
@@ -1,35 +1,42 @@
 package com.ndv.layouts
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 
-class GuiElements : AppCompatActivity() {
+class CustomOnItemSelectedListener(
+    private val applicationContext: Context,
+    private val spinner: Spinner
+) : AdapterView.OnItemSelectedListener {
+
     private fun showToast(text: String) {
         val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_LONG)
         toast.show()
     }
+
+    override fun onItemSelected(
+        parent: AdapterView<*>,
+        view: View,
+        position: Int,
+        id: Long
+    ) {
+        val text = "I like ${spinner.selectedItem} too!";
+        showToast(text)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
+}
+
+class GuiElements : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gui_elements)
 
         val spinner = findViewById<Spinner>(R.id.spinner)
-
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
-                val text = "I like ${spinner.selectedItem} too!";
-                showToast(text)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
+        spinner.onItemSelectedListener = CustomOnItemSelectedListener(applicationContext, spinner)
     }
 }
