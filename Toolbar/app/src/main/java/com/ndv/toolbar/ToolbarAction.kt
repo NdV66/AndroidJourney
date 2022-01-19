3bfc1286e721f7d864b10df432e311395e27d7bf
Marta Zazlak <marta.zazlak@hotmail.com>
2022-01-19 14:44:56 +0100

    initial

--- Toolbar/app/src/main/java/com/ndv/toolbar/ToolbarAction.kt
+++ Toolbar/app/src/main/java/com/ndv/toolbar/ToolbarAction.kt
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class ToolbarAction : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_toolbar_action)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
     }
 }
-
