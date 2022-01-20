3984e413c3181d012151a310f04f55c44c6cf971
Marta Zazlak <marta.zazlak@hotmail.com>
2022-01-20 16:29:30 +0100

    action

--- Toolbar/app/src/main/java/com/ndv/toolbar/MainActivity.kt
+++ Toolbar/app/src/main/java/com/ndv/toolbar/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.ndv.toolbar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +14,22 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.addStuff -> {
+                val intent = Intent(this, AddStuffActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
