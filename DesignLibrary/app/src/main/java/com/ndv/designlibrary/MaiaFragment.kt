d1dbb93e5749b15c67baccaf512d2478ee8b67d5
Marta Zazlak <marta.zazlak@hotmail.com>
2022-02-10 16:03:39 +0100

    init

---
+++ DesignLibrary/app/src/main/java/com/ndv/designlibrary/MaiaFragment.kt
@@ -0,0 +1,34 @@
+package com.ndv.designlibrary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+
+
+class MaiaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_maia, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupList()
+    }
+
+    private fun setupList() {
+        val view = requireView()
+        val list = view.findViewById<ListView>(R.id.maiaList)
+
+        list.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, MAIA)
+    }
+
+}
