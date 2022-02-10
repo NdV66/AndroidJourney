32fefe85a74acd7cda8cf625bba7fc477d24778c
Marta Zazlak <marta.zazlak@hotmail.com>
2022-02-10 16:56:05 +0100

    init

--- DesignLibrary/app/src/main/java/com/ndv/designlibrary/MainActivity.kt
+++ DesignLibrary/app/src/main/java/com/ndv/designlibrary/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.ndv.designlibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+
+val SCREENS = mapOf(
+    0 to StartFragment(),
+    1 to MaiaFragment(),
+    2 to ValarFragment()
+)
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.pager)
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
+    }
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = SCREENS.size
+        override fun createFragment(position: Int): Fragment = SCREENS[position]!!
     }
 }
