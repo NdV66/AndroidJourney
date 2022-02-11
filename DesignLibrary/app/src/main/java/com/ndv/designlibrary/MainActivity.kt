6d0d795be19c459597c3363338dfbab10f632724
Marta Zazlak <marta.zazlak@hotmail.com>
2022-02-11 10:40:45 +0100

    add cards with viewPager2

--- DesignLibrary/app/src/main/java/com/ndv/designlibrary/MainActivity.kt
+++ DesignLibrary/app/src/main/java/com/ndv/designlibrary/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 val SCREENS = mapOf(
     0 to StartFragment(),
@@ -13,18 +15,36 @@ val SCREENS = mapOf(
     2 to ValarFragment()
 )
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var viewPager: ViewPager2
+val CARD_TITLES = mapOf(
+    0 to "Start",
+    1 to "Maia",
+    2 to "Valar"
+)
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewPager = findViewById(R.id.pager)
-        val pagerAdapter = ScreenSlidePagerAdapter(this)
-        viewPager.adapter = pagerAdapter
+        val viewPager = prepareViewPager()
+        setupCards(viewPager)
     }
 
+    //PRIVATE
+    private fun prepareViewPager() :ViewPager2{
+        val pager = findViewById<ViewPager2>(R.id.pager)
+        pager.adapter = ScreenSlidePagerAdapter(this)
+        return pager;
+    }
+
+    private fun setupCards(viewPager: ViewPager2) {
+        val tabLayout = findViewById<TabLayout>(R.id.tabs)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = CARD_TITLES[position]
+        }.attach()
+    }
+
+    //INNER
     private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
         override fun getItemCount(): Int = SCREENS.size
         override fun createFragment(position: Int): Fragment = SCREENS[position]!!
