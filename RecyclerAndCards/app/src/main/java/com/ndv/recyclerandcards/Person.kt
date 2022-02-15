d7b16ecff47ba5a41769a656091e7658bd310137
Marta Zazlak <marta.zazlak@hotmail.com>
2022-02-15 16:08:20 +0100

    card

--- RecyclerAndCards/app/src/main/java/com/ndv/recyclerandcards/Person.kt
+++ RecyclerAndCards/app/src/main/java/com/ndv/recyclerandcards/Person.kt
@@ -1,3 +1,7 @@
 package com.ndv.recyclerandcards
 
-data class Person (val name: String, val imageId: Int = R.drawable.blank_avatar)  {}
+data class Person (
+    val name: String,
+    val imageId: Int = R.drawable.blank_avatar
+)  {}
+
