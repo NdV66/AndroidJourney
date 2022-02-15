30978a1cc9e577ed943f66142b298fc194c203e9
Marta Zazlak <marta.zazlak@hotmail.com>
2022-02-15 14:39:14 +0100

    initial

---
+++ RecyclerAndCards/app/src/main/java/com/ndv/recyclerandcards/Person.kt
@@ -0,0 +1,3 @@
+package com.ndv.recyclerandcards
+
+data class Person (val name: String, val imageId: Int = R.drawable.blank_avatar)  {}
---
+++ RecyclerAndCards/app/src/main/java/com/ndv/recyclerandcards/data.kt
@@ -0,0 +1,18 @@
+package com.ndv.recyclerandcards
+
+val VALAR = arrayOf(
+    Person("Manwë"),
+    Person("Varda"),
+    Person("Ulmo"),
+    Person("Yavanna"),
+    Person("Aulë"),
+    Person("Namo"),
+    Person("Nienna"),
+    Person("Oromë"),
+    Person("Irmo"),
+    Person("Nienna"),
+    Person("Nessa"),
+    Person("Tulkas"),
+    Person("Vana"),
+    Person("Este"))
+
