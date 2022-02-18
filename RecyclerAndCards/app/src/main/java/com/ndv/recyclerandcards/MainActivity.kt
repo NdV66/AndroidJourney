37fed2fc08660d1fa23b91cf363d12a45a98b42f
Marta Zazlak <marta.zazlak@hotmail.com>
2022-02-18 12:35:45 +0100

    recyclerview

--- RecyclerAndCards/app/src/main/java/com/ndv/recyclerandcards/MainActivity.kt
+++ RecyclerAndCards/app/src/main/java/com/ndv/recyclerandcards/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-const val MAX_IN_ROW = 3
+const val MAX_IN_ROW = 2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val recyclerView: RecyclerView = findViewById(R.id.recycler)
-        recyclerView.layoutManager = GridLayoutManager(baseContext, MAX_IN_ROW)
+        recyclerView.layoutManager = GridLayoutManager(this, MAX_IN_ROW)
         recyclerView.adapter = PersonRecyclerAdapter(VALAR)
     }
 }
