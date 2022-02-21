59492775408d59579fc63ae480f200ecee0c4d03
Marta Zazlak <marta.zazlak@hotmail.com>
2022-02-21 14:12:44 +0100

    basics

--- NavigationItems/app/src/main/java/com/ndv/navigationitems/HelpActivity.kt
+++ NavigationItems/app/src/main/java/com/ndv/navigationitems/HelpActivity.kt
@@ -1,7 +1,7 @@
 package com.ndv.navigationitems
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class HelpActivity : AppCompatActivity() {
@@ -10,9 +10,6 @@ class HelpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_help)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
-        if (toolbar !== null) {
-            setSupportActionBar(toolbar)
-        }
+        setSupportActionBar(toolbar)
     }
 }
--- NavigationItems/app/src/main/java/com/ndv/navigationitems/MainActivity.kt
+++ NavigationItems/app/src/main/java/com/ndv/navigationitems/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.ndv.navigationitems
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupToolbar()
+        showFirstFragment()
+    }
+
+    private fun showFirstFragment() {
+        val fragment = ValarFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.contentFrame, fragment)
+        transaction.commit()
+    }
+
+    private fun setupToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
