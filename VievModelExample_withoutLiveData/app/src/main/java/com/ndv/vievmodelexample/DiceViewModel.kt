2c8d9ece8f870a0a4dec1859774deb0d8dd8b8dc
Marta Zazlak <marta.zazlak@hotmail.com>
2022-03-16 17:05:48 +0100

    view model - usage

---
+++ VievModelExample_withoutLiveData/app/src/main/java/com/ndv/vievmodelexample/DiceViewModel.kt
@@ -0,0 +1,36 @@
+package com.ndv.vievmodelexample
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val MIN_DICE = 1
+
+class DiceViewModel : ViewModel() {
+    init {
+        Log.d("DiceRoll", "DiceViewModel created!")
+    }
+
+    private var _rollResult = 0
+    private var _rollsCount = 0
+
+    val rollResult: Int
+        get() = _rollResult
+
+    val rollsCount: Int
+        get() = _rollsCount
+
+    fun rollDice(dice: Int) {
+        _rollResult =  (MIN_DICE until dice).random()
+        _rollsCount++
+    }
+
+    fun reinitializeData() {
+        _rollResult = 0
+        _rollsCount = 0
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("DiceRoll", "DiceViewModel destroyed!")
+    }
+}
--- startedServices/app/src/main/java/com/ndv/startedservices/DelayedMessageWorkManager.kt
+++
@@ -1,4 +0,0 @@
-package com.ndv.startedservices
-
-class DelayedMessageWorkManager {
-}
