09b640f06c4b447f6f67e3357f19c0f137c8330f
Marta Zazlak <marta.zazlak@hotmail.com>
2022-04-06 13:21:38 +0200

    locations

--- fellowshipJourney/app/src/main/java/com/ndv/fellowshipjourney/MainActivity.kt
+++ fellowshipJourney/app/src/main/java/com/ndv/fellowshipjourney/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.ndv.fellowshipjourney
 
+import android.Manifest
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 const val DELAY_MS: Long = 1000
+const val PERMISSION_CODE = 698
 
 class MainActivity : AppCompatActivity() {
     private val serviceConnection = prepareServiceConnection()
@@ -29,8 +34,36 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        val intent = Intent(this, RoadService::class.java)
-        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSION_CODE)
+
+        } else {
+            val intent = Intent(this, RoadService::class.java)
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        println(">>>>>>>>>>>>>>>> <<<<<<<<<<<<<<< >>>>>>>>>>>>>>>>>>>>>>>> <<<<<<<<<<<<<<<< $requestCode")
+        println(grantResults[0])
+
+        if (grantResults.isNotEmpty()) {
+            val intent = Intent(this, RoadService::class.java)
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        }
     }
 
     override fun onDestroy() {
@@ -62,9 +95,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun showJourneyDistance() {
         val mainHandler = Handler(Looper.getMainLooper())
-        val task = object: Runnable {
+        val task = object : Runnable {
             override fun run() {
-                if(bound) {
+                if (bound) {
                     val distance = roadService.getDistance().toString()
                     setDistanceText(distance)
                 }
